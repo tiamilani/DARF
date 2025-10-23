8bdd6c9aed52464b234f7ed5b35be178b1552538
Tia <mattia.milani@nokia.com>
2025-10-23 15:25:05 +0200

    Updated licence and revision documentation - example

--- setup.py
+++ setup.py
@@ -1,17 +1,8 @@
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# © 2025 Nokia
+# Licensed under the BSD 3-Clause License
+# SPDX-License-Identifier: BSD-3-Clause
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# Copyright (C) 2021 Mattia Milani <mattia.milani@nokia.com>
+# Contact: Mattia Milani (Nokia) <mattia.milani@nokia.com>
 
 from setuptools import setup, find_packages
 
